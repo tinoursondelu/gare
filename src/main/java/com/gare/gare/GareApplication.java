0cae188e523025391a02dd96f0d70e1b58c176d4
olivier <guiotolivier84120@orange.fr>
2020-02-18 11:31:12 +0100

    Base commune de départ

---
+++ src/main/java/com/gare/gare/GareApplication.java
@@ -0,0 +1,13 @@
+package com.gare.gare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gare/gare/GareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gare.gare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
